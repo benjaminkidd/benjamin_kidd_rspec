311d59af6dec0ed4d9bd6be095767ff9802acd59
Benjamin Kidd <benkidd361@gmail.com>
2019-03-19 01:39:57 -0400

    added test instructions and interview questions

--- spec/api/api_login_spec.rb
+++ spec/api/api_login_spec.rb
@@ -1,7 +1,7 @@
 describe 'Shipt Login with API calls' do
   url =  'https://api.shipt.com/auth/v2/oauth/token?white_label_key=shipt'
   headers = {:x_user_type => 'Customer', :accept => 'application/x-www-form-urlencoded', :content_type => 'application/x-www-form-urlencoded'}
-  body = {:username => 'benkidd@live.com', :password => 'Spt112@32'}
+  body = {:username => 'test@shipt.com', :password => 'password@1'}
   request = RestClient::Request.new(method: :post, url: url, payload: body, headers: headers).execute
   expect(request.body).to have "Invalid Username or Password"
 end
